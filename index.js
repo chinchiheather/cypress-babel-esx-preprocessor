d61d7290b1ce876db41d5dd22bc5d303c896283d
Heather Roberts <heather.roberts@3plearning.com>
2018-04-06 15:01:58 +1000

    Allow passing through of additional presets & plugins

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const browserify = require('@cypress/browserify-preprocessor');
 
-const process = (extraPresets = [], options = browserify.defaultOptions) => {
+const preprocessor = (babelOptions = {}, options = browserify.defaultOptions) => {
     return (file) => {
-        const presets = ['babel-preset-stage-1', ...extraPresets].map(require.resolve);
+        let presets = ['babel-preset-env', 'babel-preset-stage-1'];
+        if (babelOptions.presets) {
+            presets = [...presets, babelOptions.presets];
+        }
+        presets = presets.map(require.resolve);
+
+        let plugins = [];
+        if (babelOptions.plugins) {
+            plugins = babelOptions.plugins;
+        }
+
         options.browserifyOptions.transform[1][1].presets = presets;
+        options.browserifyOptions.transform[1][1].plugins = plugins;
         return browserify(options)(file);
     };
 };
 
-module.exports = process;
+module.exports = preprocessor;
