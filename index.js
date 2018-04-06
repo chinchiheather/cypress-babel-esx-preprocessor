36449f483f24bf727ac85bc64e55ee4338ada7cb
Heather Roberts <heather.roberts@3plearning.com>
2018-04-06 15:20:49 +1000

    Add eslint

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const browserify = require('@cypress/browserify-preprocessor');
 
-const preprocessor = (babelOptions = {}, options = browserify.defaultOptions) => {
-    return (file) => {
-        let presets = ['babel-preset-env', 'babel-preset-stage-1'];
-        if (babelOptions.presets) {
-            presets = [...presets, babelOptions.presets];
-        }
-        presets = presets.map(require.resolve);
+const preprocessor = (babelOptions = {}, options = browserify.defaultOptions) => (file) => {
+  let presets = ['babel-preset-env', 'babel-preset-stage-1'];
+  if (babelOptions.presets) {
+    presets = [...presets, babelOptions.presets];
+  }
+  presets = presets.map(require.resolve);
 
-        let plugins = [];
-        if (babelOptions.plugins) {
-            plugins = babelOptions.plugins;
-        }
+  const plugins = babelOptions.plugins || [];
 
-        options.browserifyOptions.transform[1][1].presets = presets;
-        options.browserifyOptions.transform[1][1].plugins = plugins;
-        return browserify(options)(file);
-    };
+  options.browserifyOptions.transform[1][1].presets = presets;
+  options.browserifyOptions.transform[1][1].plugins = plugins;
+  return browserify(options)(file);
 };
 
 module.exports = preprocessor;
